7e4087d8687e646e16797e3774a24301c7f057b6
JSuggu <azamefranco@gmail.com>
2023-01-21 12:29:23 -0300

    eureka 2

--- customer-service/src/main/java/com/example/customerservice/feignclient/BikeFeignClient.java
+++ customer-service/src/main/java/com/example/customerservice/feignclient/BikeFeignClient.java
@@ -2,19 +2,16 @@ package com.example.customerservice.feignclient;
 
 import com.example.customerservice.model.Bike;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name="bike-service", url="http://localhost:8003/bike")
+@FeignClient(name="bike-service")
 public interface BikeFeignClient {
 
-    @PostMapping()
+    @PostMapping("/bike")
     Bike save(@RequestBody Bike bike);
 
-    @GetMapping("/byuser/{userId}")
+    @GetMapping("/bike/byuser/{userId}")
     List<Bike> getBikes(@PathVariable("userId") int userId);
 }
--- customer-service/src/main/java/com/example/customerservice/feignclient/CarFeignClient.java
+++ customer-service/src/main/java/com/example/customerservice/feignclient/CarFeignClient.java
@@ -2,19 +2,16 @@ package com.example.customerservice.feignclient;
 
 import com.example.customerservice.model.Car;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name="car-service", url="http://localhost:8002/car")
+@FeignClient(name="car-service")
 public interface CarFeignClient {
 
-    @PostMapping()
+    @PostMapping("/car")
     Car save(@RequestBody Car car);
 
-    @GetMapping("/byuser/{userId}")
+    @GetMapping("/car/byuser/{userId}")
     List<Car> getCars(@PathVariable("userId") int userId);
 }
